eb93a4a5a64f3c195168b725a644179131c7b805
Joshua Zenn <wordman05@gmail.com>
2021-09-24 22:40:10 -0400

    Escape DB password

--- pkg/db.go
+++ pkg/db.go
@@ -5,12 +5,13 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"os"
+	"net/url"
 )
 
 func GetDB() (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(fmt.Sprintf("postgresql://%s:%s@%s:5432/%s?sslmode=disable",
 		os.Getenv("RSSMQ_DB_USER"),
-		os.Getenv("RSSMQ_DB_PASSWORD"),
+		url.QueryEscape(os.Getenv("RSSMQ_DB_PASSWORD")),
 		os.Getenv("RSSMQ_DB_HOST"),
 		os.Getenv("RSSMQ_DB_DATABASE"))))
 	if err != nil {
