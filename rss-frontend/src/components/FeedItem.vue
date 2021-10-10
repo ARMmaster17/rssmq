ded7572ee56da2ce5350df03965ef79a7ecb872d
Joshua Zenn <wordman05@gmail.com>
2021-10-10 16:05:48 -0400

    Cypress E2E and component testing

---
+++ rss-frontend/src/components/FeedItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <tr >
+    <td class="border border-green-600">{{ item.Url }}</td>
+    <td class="border border-green-600">{{ item.LastChecked }}</td>
+    <td class="border border-green-600">
+      <button class="bg-red-700" v-on:click="deleteUrl(item.ID)">Delete</button>
+    </td>
+  </tr>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "FeedItem",
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    deleteUrl(id) {
+      const url = 'http://localhost:8080/api/feed/' + id + '/delete'
+      axios.post(url, {})
+          .then((response) => {
+            console.log(response)
+            this.$emit('invalidate', 'delete')
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
